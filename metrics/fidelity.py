4ff02d30d8a3449b6d7de2d234f16cf9abffc484
pierre-pauchet <pi.pauch@gmail.com>
2025-07-02 15:03:42 +0200

    test push

--- metrics/fidelity.py
+++ metrics/fidelity.py
@@ -1,3 +1,15 @@
 import rdkit
 import os
 import skpf
+
+
+if __name__ == "__main__":
+    # from skfp.fingerprints import AtomPairFingerprint
+    # from qm9.rdkit_functions import smiles_to_mols, build_molecule, build_xae_molecule
+
+    # # Example usage
+    # smiles = "CCO"
+    # mols = smiles_to_mols(smiles)
+    # fingerprints = mols_to_fingerprints(mols)
+    # print(f"Fingerprints for {smiles}: {fingerprints}")
+    print("This is a sandbox for testing and debugging purposes.")
